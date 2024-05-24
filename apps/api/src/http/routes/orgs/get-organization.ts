7baf55c81b20de5daebe31efd9dde8b78692cb17
Pedro Henrique Bérgamo <pedroh.bergamo20@gmail.com>
2024-05-24 13:18:28 -0300

    ✨ feat: Rotas: Listagem e detalhe da organização

--- apps/api/src/http/routes/orgs/get-organization.ts
+++ apps/api/src/http/routes/orgs/get-organization.ts
@@ -1,29 +1,34 @@
 import { auth } from "@/http/middlewares/auth";
-import { role } from "@next-saas-rbac/auth";
 import type { FastifyInstance } from "fastify";
 import type { ZodTypeProvider } from "fastify-type-provider-zod";
 import z from "zod";
 
-export async function getMembership(app: FastifyInstance) {
+export async function getOrganization(app: FastifyInstance) {
   app
     .withTypeProvider<ZodTypeProvider>()
     .register(auth)
     .get(
-      "/organizations/:slug/membership",
+      "/organizations/:slug",
       {
         schema: {
           tags: ["organizations"],
-          summary: "Get user membership on organization.",
+          summary: "Get details from organization.",
           security: [{ bearer: [] }],
           params: z.object({
             slug: z.string(),
           }),
           response: {
             200: z.object({
-              membership: z.object({
+              organization: z.object({
                 id: z.string().uuid(),
-                role,
-                organizationId: z.string().uuid(),
+                name: z.string(),
+                slug: z.string(),
+                domain: z.string().nullable(),
+                shouldAttachUsersByDomain: z.boolean(),
+                avatarUrl: z.string().url().nullable(),
+                createdAt: z.date(),
+                updatedAt: z.date(),
+                ownerId: z.string().uuid(),
               }),
             }),
           },
@@ -31,15 +36,10 @@ export async function getMembership(app: FastifyInstance) {
       },
       async (request, reply) => {
         const { slug } = request.params;
-
-        const { membership } = await request.getUserMembership(slug);
+        const { organization } = await request.getUserMembership(slug);
 
         return reply.status(200).send({
-          membership: {
-            id: membership.id,
-            role: membership.role,
-            organizationId: membership.organizationId,
-          },
+          organization,
         });
       }
     );
