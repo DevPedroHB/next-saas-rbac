5ba5b29e5cebb719bf377d2dbb9eb19620ce3e77
Pedro Henrique Bérgamo <pedroh.bergamo20@gmail.com>
2025-06-01 01:23:19 -0300

    ✨ feat: Adicionar middleware de autenticação e método para obter ID do usuário autenticado

--- apps/api/src/http/routes/auth/get-profile.ts
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -1,3 +1,4 @@
+import { auth } from "@/http/middlewares/auth";
 import { prisma } from "@next-saas-rbac/database";
 import type { FastifyInstance } from "fastify";
 import type { ZodTypeProvider } from "fastify-type-provider-zod";
@@ -5,46 +6,49 @@ import { z } from "zod";
 import { UnauthorizedError } from "../errors/unauthorized-error";
 
 export async function getProfile(app: FastifyInstance) {
-	app.withTypeProvider<ZodTypeProvider>().get(
-		"/profile",
-		{
-			schema: {
-				summary: "Get authenticated user profile",
-				tags: ["auth"],
-				operationId: "getProfile",
-				params: z.object({}),
-				response: {
-					200: z.object({
-						user: z.object({
-							id: z.string(),
-							name: z.string().nullable(),
-							email: z.string(),
-							avatarUrl: z.string().nullable(),
+	app
+		.withTypeProvider<ZodTypeProvider>()
+		.register(auth)
+		.get(
+			"/profile",
+			{
+				schema: {
+					summary: "Get authenticated user profile",
+					tags: ["auth"],
+					operationId: "getProfile",
+					params: z.object({}),
+					response: {
+						200: z.object({
+							user: z.object({
+								id: z.string(),
+								name: z.string().nullable(),
+								email: z.string(),
+								avatarUrl: z.string().nullable(),
+							}),
 						}),
-					}),
+					},
 				},
 			},
-		},
-		async (request, reply) => {
-			const { sub } = await request.jwtVerify<{ sub: string }>();
+			async (request, reply) => {
+				const userId = await request.getCurrentUserId();
 
-			const user = await prisma.user.findUnique({
-				select: {
-					id: true,
-					name: true,
-					email: true,
-					avatarUrl: true,
-				},
-				where: { id: sub },
-			});
+				const user = await prisma.user.findUnique({
+					select: {
+						id: true,
+						name: true,
+						email: true,
+						avatarUrl: true,
+					},
+					where: { id: userId },
+				});
 
-			if (!user) {
-				throw new UnauthorizedError();
-			}
+				if (!user) {
+					throw new UnauthorizedError();
+				}
 
-			return reply.status(200).send({
-				user,
-			});
-		},
-	);
+				return reply.status(200).send({
+					user,
+				});
+			},
+		);
 }
